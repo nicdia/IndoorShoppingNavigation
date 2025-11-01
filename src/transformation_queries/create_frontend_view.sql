f8896dca1372f280b52d91902c7c700870664712
Nicolas Diaczyszyn <nicodia99@gmail.com>
2025-11-01 13:03:22 +0100

    removed empty spaces and deleted irrelevant col from final view

--- src/transformation_queries/create_frontend_view.sql
+++ src/transformation_queries/create_frontend_view.sql
@@ -1,12 +1,9 @@
--- Alle Produkte mit Koordinaten (für Map & Suche)
 CREATE VIEW IF NOT EXISTS v_product_map AS
 SELECT
   p.product_id,
-  p.product_code,
   p.product_name,
   p.product_level,
-  pl.product_product_id,
-  pl.node_node_id                 AS node_id,
+  pl.node_node_id                 
   n.node_x,
   n.node_y
 FROM products p
